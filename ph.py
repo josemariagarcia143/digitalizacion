738d273398cf97aed6e844f8986c2bd5a4d7da8d
josemariagarcia143 <josemaria.garcia@colegiomolierehuelva.es>
2026-04-08 08:52:11 +0200

    Update ph.py

--- ph.py
+++ ph.py
@@ -1,11 +1,11 @@
 ph = int(input('Escribe el Ph: '))
 if ph <= 4:
     print('Muy ácido.')
-elif ph > 4 and ph < 7:
+elif 4 < ph < 7:
     print('Poco ácido.')
 elif ph == 7:
     print('Neutro.')    
-elif ph > 7 and ph <= 10:
+elif 7 < ph <= 10:
     print('Poco alcalino.')
 else:
     print('Muy alcalino.')
